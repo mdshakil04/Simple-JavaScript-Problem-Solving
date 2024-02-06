21f2ed882bf1420c4a764c666833e404ed36507c
mdshakil04 <m.shakil04@yahoo.com>
2024-02-06 19:33:04 +0600

    Temporary variable

---
+++ temp.js
@@ -0,0 +1,7 @@
+let firstName = "Mehrima";
+let lastName =" Aysha";
+console.log(firstName, lastName)
+const temp = firstName;
+firstName = lastName;
+lastName = temp;
+console.log(firstName, lastName);
