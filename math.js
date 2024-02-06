26d94a992ec195e1abc54fb7b19c4bea40d449a0
mdshakil04 <m.shakil04@yahoo.com>
2024-02-06 17:41:47 +0600

    Module-21-4 done

---
+++ math.js
@@ -0,0 +1,21 @@
+const result = Math.pow(2, 3);
+// console.log(result)
+const num1 = 18;
+const num2 = 20;
+const gap = Math.abs(num1 - num2)
+console.log(gap);
+if(gap < 5){
+    console.log("You are not cafabole to doing this")
+}
+else{
+    console.log("You can stay together")
+}
+// -------------------------------------
+const number = 3.141618;
+const result1 = Math.ceil(number);
+const result2 = Math.floor(number);
+const result3 = Math.round(number);
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(Math.round(Math.random()*100))
