8a9589fc9f06f5f49caa0dc3b450c0788050f192
mdshakil04 <m.shakil04@yahoo.com>
2024-02-07 21:38:39 +0600

    Cake Problem updated

--- cake-prbm.js
+++ cake-prbm.js
@@ -1,11 +1,15 @@
-function cakeDivide(m, n){
-    if (m > n){
-        console.log("Jim get the cake")
+function cakeDivide(m, n, o){
+    if (m > n && m > o){
+        console.log("Jim will get the cake")
+    }
+    else if(n > m && n > o){
+        console.log("Dela will get the cake")
     }
     else{
-        console.log("Dela get the cake")
+        console.log("Niha will get the cake")
     }
 }
 const jim = 84;
-const Dela = 75;
-const cake = cakeDivide(jim, Dela);
+const dela = 75;
+const niha = 98;
+const cake = cakeDivide(jim, dela, niha);
